05eab1cb91861806b10a9d6d11be9721c0e59078
zhuzheng <qiuyuewu1987@163.com>
2017-07-21 13:26:43 +0800

    to camel case

--- kebabCase.js
+++ kebabCase.js
@@ -1,5 +1,11 @@
 // 驼峰转下划线连字符
 
-function kebabCase(str) {
+export function kebabCase(str) {
 	return str.replace(/(?!^)([A-Z])/g, '_$1').toLowerCase();
+}
+
+// 转驼峰
+
+export function camelCase(str) {
+	return str.replace(/(_[\w])/g, (all, letter) => (letter.slice(1).toUpperCase()))
 }
