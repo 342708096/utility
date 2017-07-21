c896f93b8bed43d114cb58733fca493e938d16d6
zhuzheng <qiuyuewu1987@163.com>
2017-07-21 13:20:18 +0800

    cookie

--- cookie.js
+++ cookie.js
@@ -1,1 +1,35 @@
-cookie.js
+export default {
+    /**
+     * @func getCookie
+     * @desc 获取cookie字段
+     * @param {string} name - 字段名 
+     * @returns {(string|object)} 返回字段值或整个cookie对象
+     */
+    get: function (name) {
+        const cookie = {};
+        const cookieSplit = document.cookie.split('; ');
+        for (let i = 0; i < cookieSplit.length; i++) {
+            const keyValue = cookieSplit[i].split('=');
+            cookie[keyValue[0]] = decodeURIComponent(keyValue[1]);
+        }
+
+        return name ? cookie[name] : cookie;
+    },
+
+    /**
+     * @func setCookie
+     * @desc 设置cookie字段
+     * @param {string} d - cookie域名 
+     * @param {string} e - cookie参数
+     * @param {string} t - cookie值
+     * @param {number} n - 过期小时数
+     */
+    set: function (domain, key, value, hour) {
+        if (hour) {
+            const i = new Date((new Date()).getTime() + hour * 36e5);
+            document.cookie = key + "=" + escape(value) + ";path=/;domain=" + domain + ";expires=" + i.toGMTString();
+        } else {
+            document.cookie = key + "=" + escape(value) + ";path=/;domain=" + domain;
+        }
+    }
+};
